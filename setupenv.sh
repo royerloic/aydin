b371df71e9173fb80c1f69ab7558577059f1d472
Ahmet Can Solak <asolak14@ku.edu.tr>
2019-07-24 19:32:37 +0000

    removed setupenvsh

--- setupenv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-conda create -n pitl python=3.7
-conda activate pitl
-
-conda install mkl numpy zarr scipy scikit-image scikit-learn click black dask pytest numexpr pyopencl lightgbm
-
-pip install PIMS gdown tifffile czifile pre-commit black
-pip install imagecodecs
