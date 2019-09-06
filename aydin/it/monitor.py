4c5d93f520fbe37250312f7b4c0603764cf46ada
Ahmet Can Solak <asolak14@ku.edu.tr>
2019-09-06 18:32:07 +0000

    N2stestfix

--- aydin/it/monitor.py
+++ aydin/it/monitor.py
@@ -1,6 +1,10 @@
 class Monitor:
-    def __init__(self, monitoring_callbacks=[], monitoring_images=None):
-        self.monitoring_callbacks = monitoring_callbacks
+    def __init__(self, monitoring_callbacks=None, monitoring_images=None):
+        if monitoring_callbacks is None:
+            self.monitoring_callbacks = []
+        else:
+            self.monitoring_callbacks = monitoring_callbacks
+
         self.monitoring_images = monitoring_images
 
         self.variables = None
@@ -15,10 +19,11 @@ class Monitor:
     @variables.setter
     def variables(self, val):
         if val is not None:
+            # Update the values of variables to be monitored
             self._iter = val[0]
             self._val_loss = val[1]
             self._image = val[2]
-            # print("changed...")
-            # print(self.monitoring_variables)
+
+            # Make calls to callbacks accordingly
             for monitoring_callback in self.monitoring_callbacks:
                 monitoring_callback((self._iter, self._val_loss, self._image))
