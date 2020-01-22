cf7579352709be00b5ddf074332f91d9c33d0892
Ahmet Can Solak <asolak14@ku.edu.tr>
2020-01-22 22:35:35 +0000

    disabling translation

--- aydin/services/n2t.py
+++ aydin/services/n2t.py
@@ -1,60 +1,64 @@
 from aydin.it.monitor import Monitor
 from aydin.services.base import BaseService
 
-
+#
+#
 class N2TService(BaseService):
-    """Noise2Self Service.
-    """
+    pass
 
-    def __init__(self, backend_preference=None, scales=None, widths=None):
-        super(N2TService, self).__init__(
-            backend_preference=backend_preference, scales=scales, widths=widths
-        )
 
-    def run(
-        self,
-        noisy_image,
-        truth_image,
-        test_image,
-        progress_callback,
-        monitoring_callbacks=None,
-        monitoring_images=None,
-        generator=None,
-        regressor=None,
-    ):
-        """
-        Method to run Noise2Self service
-
-        :param regressor:
-        :param generator:
-        :param test_image:
-        :param truth_image:
-        :param monitoring_callbacks:
-        :param monitoring_images:
-        :param progress_callback:
-        :param self:
-        :param noisy_image: input noisy image, must be np compatible
-        :return: denoised version of the input image, will be np compatible
-        """
-        self.set_image_metrics(noisy_image.shape)
-        progress_callback.emit(5)
-
-        generator = generator if generator is not None else self.get_generator()
-        regressor = regressor if regressor is not None else self.get_regressor()
-        progress_callback.emit(41)
-
-        self.it = self.get_translator(
-            feature_generator=generator,
-            regressor=regressor,
-            normaliser_type='percentile',
-            monitor=Monitor(
-                monitoring_callbacks=monitoring_callbacks,
-                monitoring_images=monitoring_images,
-            ),
-        )
-        self.it.train(noisy_image, truth_image)
-        progress_callback.emit(80)
-
-        response = self.it.translate(test_image)
-        progress_callback.emit(100)
-        return response
+#     """Noise2Self Service.
+#     """
+#
+#     def __init__(self, backend_preference=None, scales=None, widths=None):
+#         super(N2TService, self).__init__(
+#             backend_preference=backend_preference, scales=scales, widths=widths
+#         )
+#
+#     def run(
+#         self,
+#         noisy_image,
+#         truth_image,
+#         test_image,
+#         progress_callback,
+#         monitoring_callbacks=None,
+#         monitoring_images=None,
+#         generator=None,
+#         regressor=None,
+#     ):
+#         """
+#         Method to run Noise2Self service
+#
+#         :param regressor:
+#         :param generator:
+#         :param test_image:
+#         :param truth_image:
+#         :param monitoring_callbacks:
+#         :param monitoring_images:
+#         :param progress_callback:
+#         :param self:
+#         :param noisy_image: input noisy image, must be np compatible
+#         :return: denoised version of the input image, will be np compatible
+#         """
+#         self.set_image_metrics(noisy_image.shape)
+#         progress_callback.emit(5)
+#
+#         generator = generator if generator is not None else self.get_generator()
+#         regressor = regressor if regressor is not None else self.get_regressor()
+#         progress_callback.emit(41)
+#
+#         self.it = self.get_translator(
+#             feature_generator=generator,
+#             regressor=regressor,
+#             normaliser_type='percentile',
+#             monitor=Monitor(
+#                 monitoring_callbacks=monitoring_callbacks,
+#                 monitoring_images=monitoring_images,
+#             ),
+#         )
+#         self.it.train(noisy_image, truth_image)
+#         progress_callback.emit(80)
+#
+#         response = self.it.translate(test_image)
+#         progress_callback.emit(100)
+#         return response
